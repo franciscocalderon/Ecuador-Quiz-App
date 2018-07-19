9d2b198bb299616d1a0e5ed653b6ee0763ecdffe
Francisco Calderon <francisco.calderon.hervas@gmail.com>
2018-07-19 18:41:37 -0500

    First working version

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,12 @@
 var questionIndex;
 var questionSet;
 var currentQuestion;
-
+var correctCount;
+const CORRECT_IMG_URL = "images/green-check.png";
+const WRONG_IMG_URL = "images/x-mark.png";
+const SUBMIT_BTN_TEXT = "Submit";
+const NEXT_BTN_TEXT = "Next Question";
+const START_BTN_TEXT = "Start Quiz";
 
 var getOptionHTML = function(option){
 	return (`<div class="col-6">
@@ -21,47 +26,146 @@ var getQuestionHTML = function(question){
 	questionHTML += '</div><div class="inner-row">';
 	questionHTML += optionsHTML.shift();
 	questionHTML += optionsHTML.shift();
-	questionHTML += '</div></div></div>'
+	questionHTML += '</div></div></div>';
 	return questionHTML;
 }
 
-
 var addClickOptionListener = function(){
-	$(".quiz-frame").on('click', '.option', function(event){
+	$(".question-info").on('click', '.option', function(event){
 		$(".option").removeClass("highlighted");
 		$(this).addClass("highlighted");
 	});
 }
 
-var i = 0;
+var nextQuestion = function(){
+	if(questionIndex < (questionSet.length - 1)){
+		currentQuestion = questionSet[++questionIndex];
+		displayQuestion();
+		setButtonText(SUBMIT_BTN_TEXT);
+		updateProgress();
+	}
+	else{
+		displayResults();
+		setButtonText(START_BTN_TEXT);
+	}
+}
+
+var setUpDefaultQuiz = function(){
+	setUpQuiz(QUESTIONS);
+}
+
+var startQuiz = function(){
+	nextQuestion();
+	var button = $(".quiz-btn span");
+	button.text(SUBMIT_BTN_TEXT);
+}
+
+var getSelectedOption = function(){
+	return $('.highlighted span').text();
+}
+
+var getButtonText = function(){
+	return $(".quiz-btn span").text();
+}
+
+var setButtonText = function(newText){
+	$(".quiz-btn span").text(newText);
+}
+
 var addSubmitListener = function(){
 	$(".question").submit(function(event){
 		event.preventDefault();
-		var selectedOption = $('.highlighted');
-		$(".quiz-test .question-info").html(getQuestionHTML(QUESTIONS[i++]));
+		var buttonText = getButtonText();
+		if(buttonText === SUBMIT_BTN_TEXT){
+			var selectedOption = getSelectedOption();
+			if(selectedOption)
+				displayFeedback();
+			else
+				alert("You need to select an option first!");
+		}
+
+		else if(buttonText === NEXT_BTN_TEXT)
+			nextQuestion();
+
+		else if(buttonText === START_BTN_TEXT){
+			setUpDefaultQuiz();
+			startQuiz();
+		}
+
+
+
 	});
+}
+
+var toggleButtonText = function(){
+	var buttonText = $(".quiz-btn span");
+	console.log(buttonText.text());
+	if(buttonText.text() === "Submit")
+		buttonText.text("Next Question");
+	else if(buttonText.text() === "Next Question")
+		buttonText.text("Submit");
 }
 
 var addAllListeners = function(){
 	addClickOptionListener();
 	addSubmitListener();
+	setUpQuiz(QUESTIONS);
 }
 
 var setUpQuiz = function(questions){
 	questionSet = questions;
 	questionIndex = -1;
+	correctCount = 0;
+}
+
+//TODO RESULTS PAGE
+var getResultsHTML = function(){
+	return (`<h2>Quiz Finished!</h2>
+			<div class="feedback-txt">You got <span class="bold">` + correctCount 
+			+ `</span> questions right out of <span class="bold">` + questionSet.length 
+			+ `</span></div><div class="feedback-txt">Want to give it another go? Click on 'Start Quiz' below!</div>`);
 }
 
 var displayResults = function(){
+	clearProgress();
 	var resultsHTML = getResultsHTML();
-	$('.quiz-frame').html(resultsHTML);
+	$('.question-info').html(resultsHTML);
 }
 
-var nextQuestion = function(){
-	if(questionIndex < (questionSet.length - 1))
-		currentQuestion = questionSet[++questionIndex];
-	else
-		displayResults();
+var displayQuestion = function(){
+	$('.question-info').html(getQuestionHTML(currentQuestion));
+}
+
+var getFeedBackHTML = function(isCorrect){
+	var correctness = isCorrect ? "Correct!" : "Incorrect!";
+	var imgpath = isCorrect ? CORRECT_IMG_URL : WRONG_IMG_URL;
+
+
+	return ('<h3>' + correctness + '</h3>'
+		+ '<div class="feedback-txt">The correct answer was: <span class="bold">' + currentQuestion.correctOption + '</span></div>'
+		+ '<img class="feedback-img" src="' +imgpath + '"">'
+		+ '<div class="feedback-txt">' + currentQuestion.feedback + '</div>');
+}
+
+var displayFeedback = function(){
+	var isCorrectAnswer = (getSelectedOption() === currentQuestion.correctOption);
+	if(isCorrectAnswer)
+		correctCount++;
+	$('.question-info').html(getFeedBackHTML(isCorrectAnswer));
+	setButtonText(NEXT_BTN_TEXT);
+}
+
+var getProgressHTML = function(){
+	return ('<div class="col-6">Question ' + (questionIndex+1) + ' of ' + questionSet.length
+				+ '</div><div class="col-6">' + correctCount + ' Correct</div>');
+}
+
+var updateProgress = function(){
+	$('.progress').html(getProgressHTML());
+}
+
+var clearProgress = function(){
+	$('.progress').html("");
 }
 
 $(addAllListeners);
--- scripts/questionbank.js
+++ scripts/questionbank.js
@@ -3,50 +3,60 @@ const QUESTIONS = [
 		text: "What is the capital city of Ecuador?",
 		options: ["Guayaquil", "Quito", "Cuenca", "Ambato"],
 		correctOption: "Quito",
+		feedback: "Located in the northern Sierra region, Quito was founded in 1531",
 	},
 	{
 		text: "What is the highest mountain/volcano of Ecuador?",
 		options: ["Sangay", "Cotopaxi", "Chimborazo", "Cayambe"],
-		correctOption: "Quito",
+		correctOption: "Chimborazo",
+		feedback: "With an elevation of 6,263 m (20,549 ft), the inactive volcano Chimborazo is the highest mountain of Ecuador and the farthest point from the center of the Earth (of the whole world!)",
 	},
 	{
 		text: "Which is the most populous city of Ecuador?",
 		options: ["Guayaquil", "Quito", "Cuenca", "Ambato"],
 		correctOption: "Guayaquil",
+		feedback: "Closely followed by Quito, Guayaquil is the most populous city of Ecuador, with 2'644,891 inhabitants"
 	},
 	{
 		text: "Other than Spanish, which of the following is considered an official language in Ecuador?",
 		options: ["Quichua", "Aymara", "Guaraní", "Nahuatl"],
 		correctOption: "Quichua",
+		feedback: "Mostly spoken by indigenous communities, Quichua is recognized as an official language in the Ecuadorian Constitution",
 	},
 	{
 		text: "Which of the following islands/archipielagos are a province of Ecuador?",
 		options: ["Easter Islands", "Coco Islands", "Guayas Islands", "Galapagos Islands"],
 		correctOption: "Galapagos Islands",
+		feedback: "Annexed to Ecuadorian territory in 1832, the Galapagos Islands are known worldwide for their endemic species and their contribution to Darwin's theory of evolution",
 	},
 	{
 		text: "How many provinces does Ecuador have?",
 		options: ["16", "31", "9", "24"],
 		correctOption: "24",
+		feedback: "Ecuador has 24 provinces as part of its administrative division, with Santa Elena and Santo Domingo de los Tsáchilas being the newest ones, established in 2007",
 	},
 	{
 		text: "What are the colors of the Ecuadorian Flag?",
 		options: ["Yellow - Blue - Red", "Red - White", "Yellow - Red - Green", "Green - Yellow - Blue"],
 		correctOption: "Yellow - Blue - Red",
+		feedback: "The Ecuadorian Flag shares its color and distinctive pattern with the flags of Colombia and Venezuela, and features the Ecuadorian Coat of Arms in the center"
 	},
 	{
 		text: "What is the most popular sport in Ecuador?",
 		options: ["Ecuavolley", "Basketball", "Soccer", "Rugby"],
 		correctOption: "Soccer",
+		feedback: "Enjoyed everywhere within the country, soccer (fútbol) is by far the most practiced sport and the one with the biggest following",
 	},
 	{
 		text: "How many main regions does Ecuador have?",
 		options: ["4", "2", "7", "5"],
 		correctOption: "4",
+		feedback: "Ecuador has 4 main regions: Coast, Sierra or Andean Region, Amazon, and the Galapagos Islands",
 	},
 	{
 		text: "What is the current official currency of Ecuador?",
 		options: ["Sucre", "Ecuadorian Peso", "US Dollar", "Colombian Peso"],
 		correctOption: "US Dollar",
+		feedback: "The current official currency of Ecuador is the US Dollar, which replaced the Sucre in 2000",
 	}
 ];
